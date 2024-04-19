6b7e696b0f9348d8102a091c7eab9fa95cf60944
Jovan <jormundur00@gmail.com>
2024-04-19 14:15:26 +0200

    feat: Implemented authority checks for user access and update
    Added hiding of html page access elements for unauthorized users.
    Also forbade unauthorized users to access those pages using http requests.
    Added checks for REVIEWER and MANAGER users to check if they are updating themselves, disallowing them to update other users.

--- src/main/java/com/urosdragojevic/realbookstore/controller/PersonsController.java
+++ src/main/java/com/urosdragojevic/realbookstore/controller/PersonsController.java
@@ -2,14 +2,19 @@ package com.urosdragojevic.realbookstore.controller;
 
 import com.urosdragojevic.realbookstore.audit.AuditLogger;
 import com.urosdragojevic.realbookstore.domain.Person;
+import com.urosdragojevic.realbookstore.domain.Role;
 import com.urosdragojevic.realbookstore.domain.User;
 import com.urosdragojevic.realbookstore.repository.PersonRepository;
+import com.urosdragojevic.realbookstore.repository.RoleRepository;
 import com.urosdragojevic.realbookstore.repository.UserRepository;
 import jakarta.servlet.http.HttpSession;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PostAuthorize;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -26,13 +31,17 @@ public class PersonsController {
 
     private final PersonRepository personRepository;
     private final UserRepository userRepository;
+    private final RoleRepository roleRepository;
 
-    public PersonsController(PersonRepository personRepository, UserRepository userRepository) {
+
+    public PersonsController(PersonRepository personRepository, UserRepository userRepository, RoleRepository roleRepository) {
         this.personRepository = personRepository;
         this.userRepository = userRepository;
+        this.roleRepository = roleRepository;
     }
 
     @GetMapping("/persons/{id}")
+    @PreAuthorize("hasAuthority('VIEW_PERSON')")
     public String person(@PathVariable int id, Model model, HttpSession session) {
         model.addAttribute("CSRF_TOKEN", session.getAttribute("CSRF_TOKEN"));
         model.addAttribute("person", personRepository.get("" + id));
@@ -55,16 +64,31 @@ public class PersonsController {
     }
 
     @PostMapping("/update-person")
+    @PreAuthorize("hasAuthority('UPDATE_PERSON')")
     public String updatePerson(Person person, HttpSession session, @RequestParam("csrfToken") String csrfToken) throws AccessDeniedException {
         String csrf = session.getAttribute("CSRF_TOKEN").toString();
         if(!csrf.equals(csrfToken)) {
             throw new AccessDeniedException("Forbidden");
         }
-        personRepository.update(person);
-        return "redirect:/persons/" + person.getId();
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        User user = (User)authentication.getPrincipal();
+        Integer userId = user.getId();
+        List<Role> roles = roleRepository.findByUserId(userId);
+        for(Role role : roles) {
+            if (role.getName().equals("ADMIN")) {
+                personRepository.update(person);
+                return "redirect:/persons/" + person.getId();
+            } else if (userId.toString().equals(person.getId())) {
+                personRepository.update(person);
+                return "redirect:/myprofile";
+            }
+        }
+        throw new AccessDeniedException("Forbidden");
     }
 
     @GetMapping("/persons")
+    @PreAuthorize("hasAuthority('VIEW_PERSONS_LIST')")
     public String persons(Model model) {
         model.addAttribute("persons", personRepository.getAll());
         return "persons";
